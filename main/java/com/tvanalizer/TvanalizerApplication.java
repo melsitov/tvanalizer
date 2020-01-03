797ef5640cc1634ead8d617051d7be092117d765
melsitov <alexey.melsitov@gmail.com>
2020-01-03 17:42:23 +0200

    1st commit

---
+++ main/java/com/tvanalizer/TvanalizerApplication.java
@@ -0,0 +1,13 @@
+package com.tvanalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvanalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvanalizerApplication.class, args);
+	}
+
+}
---
+++ test/java/com/tvanalizer/TvanalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvanalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvanalizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
